0e8304ab8c6f99e9e9d346b66d15312d35178f0e
cjt <2283830825@qq.com>
2022-11-29 20:02:33 +0800

    菜单路由映射进度==20%

---
+++ src/router/main/analysis/dashborad/dashborad.js
@@ -0,0 +1,7 @@
+const dashboard = () => import('@/views/main/analysis/dashboard')
+export default {
+  path: '/main/analysis/dashboard',
+  name: 'dashboard',
+  component: dashboard,
+  children: []
+}
---
+++ src/router/main/analysis/overview/overview.js
@@ -0,0 +1,8 @@
+
+const overview = () => import('@/router/main/analysis/overview/overview')
+export default {
+  path: '/main/analysis/overview',
+  name: 'overview',
+  component: overview,
+  children: []
+}
